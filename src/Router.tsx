608f58f9fc118027167517a4708104f2937042a4
Orlando Gomes <orlandosjm@yahoo.com.br>
2023-02-21 15:16:15 -0300

    add routes layout

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,21 @@
 import { Routes, Route } from 'react-router-dom'
+import { DefaultLayout } from './layouts/DefaultLayout'
 import { History } from './pages/History'
 import { Home } from './pages/Home'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
+
+      {/* Ex. Mais um layout para administrador, por exemplo 
+       <Route path="/admin" element={<AdminLayout />}>
+        <Route path="products" element={<ProductsAdmin />} />
+      </Route>
+      http://localhost:3333/admin/products */}
     </Routes>
   )
 }
